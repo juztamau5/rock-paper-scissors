e279739eec889ae34f68cba76c399b63e9819936
juztamau5 <juztamau5@gmail.com>
2022-10-14 15:12:15 -0700

    Add Yarn packaging for Typescript project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2022 juztamau5
+ * This file is part of Rock Paper Scissors Dapp - https://github.com/juztamau5/rock-paper-scissors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * See LICENSE.txt for more information.
+ */
+
+// Needed until an import or export is added to make this a module
+export {};
